8275cfb2bb652159c0e1c5faa87cc6f55287e3f2
Franco Arratia <farratia@aptos.com>
2019-03-11 18:24:09 -0400

    ES6-Features: Trying to complete webpack configuration

--- ES6Examples/webpack/webpack.config.js
+++ ES6Examples/webpack/webpack.config.js
@@ -2,5 +2,17 @@ module.exports = {
     entry: './assets/js/scripts.js',
     output: {
         filename: './public/js/bundle.js'
-    }
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                query: {
+                    pressets: ['es2015']
+                }
+            }
+        ]
+    }    
 };
